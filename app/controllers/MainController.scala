23dcdee81dd763d5e3d7f43dc02df0a23d5a3207
Andrey Osmolovskiy <a.osmolovsky@promo.ua>
2017-09-11 09:05:50 +0300

    Fixed bug with actor initializing

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -2,20 +2,18 @@ package controllers
 
 import javax.inject._
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.ActorSystem
 import akka.stream.Materializer
 import helpers.implicits._
 import models.internal.Succ
 import play.api.Configuration
 import play.api.libs.ws.WSClient
 import play.api.mvc._
-import protocols.actors._
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class MainController @Inject()(@Named(PROCESSOR) processorActor: ActorRef)
-                              (cc: ControllerComponents,
+class MainController @Inject()(cc: ControllerComponents,
                                ws: WSClient)
                               (implicit val system: ActorSystem,
                                exc: ExecutionContext,
